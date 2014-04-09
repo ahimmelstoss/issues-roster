777e8efa7f8fbf5a0dad649b4e9790ce2a5bf8de
ahimmelstoss <ahimmelstoss@gmail.com>
2014-04-09 15:37:32 -0400

    generate app and sessions controller; unsure if it works

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    set_session_data
+    
+    if session['github'][:token]
+      redirect '/'
+    end
+  end
+
+  private
+    def set_session_data
+      github_data = request.env['omniauth.auth']
+      session['github'] = {
+        :uid => github_data.uid,
+        :username => github_data.info.nickname,
+        :email => github_data.info.email,
+        :token => github_data.credentials.token
+      }
+    end
+
+end
