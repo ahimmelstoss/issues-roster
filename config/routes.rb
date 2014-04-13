91940cd656d99c764c95d4fd77bd1112cea378cc
ahimmelstoss <ahimmelstoss@gmail.com>
2014-04-12 21:37:26 -0400

    added validations on params to account for naming

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get '/auth/github/callback' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   get '/organizations' => 'organizations#index', :as => 'organizations'
-  get '/organizations/:organization' => 'organizations#show', :as => 'organization'
-  get '/organizations/:organization/repos/:repo' => 'repos#show', :as => 'repo'
+  get '/organizations/:organization' => 'organizations#show', :as => 'organization', :constraints  => { :organization => /[-0-z\._]+/ }
+  get '/organizations/:organization/repos/:repo' => 'repos#show', :as => 'repo', :constraints  => { :repo => /[-0-z\._]+/, :organization => /[-0-z\._]+/ }
 end
