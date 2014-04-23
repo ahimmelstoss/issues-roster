d99bae32b038426de369ca16fb43b19566284fe3
ahimmelstoss <ahimmelstoss@gmail.com>
2014-04-23 14:53:39 -0400

    refactored github connection with Struct

--- app/models/github_connection.rb
+++ app/models/github_connection.rb
@@ -1,44 +1,39 @@
 class GithubConnection
-  attr_reader :username, :token, :orgs, :repos, :issues
-
-  #TODO: refactor repeated code
+  attr_reader :typhoeus_request, :orgs, :repos, :issues, :username, :token
 
   def initialize(github_data)
     @username = github_data["username"]
     @token = github_data["token"]
+    @typhoeus_request = TyphoeusRequest.new(username, token)
   end
 
   def get_organizations
-    request = Typhoeus::Request.new(
-      "https://api.github.com/user/orgs",
-      headers: {Authorization: "token #{token}"}
-    )
-    response = request.run
-    orgs = JSON.parse(response.body).map do |org| 
+    orgs = JSON.parse(typhoeus_request.make_request("https://api.github.com/user/orgs",
+      headers: {Authorization: "token #{token}"}).body).map do |org| 
       org["login"]
     end
   end
 
   def get_repos(organization)
-    request = Typhoeus::Request.new(
-      "https://api.github.com/orgs/#{organization}/repos",
-      headers: {Authorization: "token #{token}"}
-      )
-    response = request.run
-    repos = JSON.parse(response.body).map do |repo|
+    repos = JSON.parse(typhoeus_request.make_request("https://api.github.com/orgs/#{organization}/repos",
+      headers: {Authorization: "token #{token}"}).body).map do |repo|
       repo["name"]
     end
   end
 
   def get_issues(organization, repo)
-    request = Typhoeus::Request.new(
-      "https://api.github.com/repos/#{organization}/#{repo}/issues",
-      headers: {Authorization: "token #{token}"}
-      )
-    response = request.run
-    repos = JSON.parse(response.body).map do |issue|
+    issues = JSON.parse(typhoeus_request.make_request("https://api.github.com/repos/#{organization}/#{repo}/issues",
+      headers: {Authorization: "token #{token}"}).body).map do |issue|
       issue
     end
   end
+
+  TyphoeusRequest = Struct.new(:username, :token) do 
+    def make_request(url, header)
+      request = Typhoeus::Request.new(url, header)
+      response = request.run
+    end
+  end
+
 
 end
