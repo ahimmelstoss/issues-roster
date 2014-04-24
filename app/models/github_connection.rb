be243e2c13dc67eaad497eb8ca7af43ec92129ed
ahimmelstoss <ahimmelstoss@gmail.com>
2014-04-23 22:33:41 -0400

    total DRY refactor

--- app/models/github_connection.rb
+++ app/models/github_connection.rb
@@ -1,39 +1,32 @@
 class GithubConnection
-  attr_reader :typhoeus_request, :orgs, :repos, :issues, :username, :token
+  attr_reader :orgs, :repos, :issues, :username, :token
 
   def initialize(github_data)
     @username = github_data["username"]
     @token = github_data["token"]
-    @typhoeus_request = TyphoeusRequest.new(username, token)
   end
 
   def get_organizations
-    orgs = JSON.parse(typhoeus_request.make_request("https://api.github.com/user/orgs",
-      headers: {Authorization: "token #{token}"}).body).map do |org| 
-      org["login"]
-    end
+    orgs = json_parse("https://api.github.com/user/orgs", "login")
   end
 
   def get_repos(organization)
-    repos = JSON.parse(typhoeus_request.make_request("https://api.github.com/orgs/#{organization}/repos",
-      headers: {Authorization: "token #{token}"}).body).map do |repo|
-      repo["name"]
-    end
+    repos = json_parse("https://api.github.com/orgs/#{organization}/repos", "name")
   end
 
   def get_issues(organization, repo)
-    issues = JSON.parse(typhoeus_request.make_request("https://api.github.com/repos/#{organization}/#{repo}/issues",
-      headers: {Authorization: "token #{token}"}).body).map do |issue|
-      issue
-    end
+    issues = json_parse("https://api.github.com/repos/#{organization}/#{repo}/issues")
   end
 
-  TyphoeusRequest = Struct.new(:username, :token) do 
-    def make_request(url, header)
-      request = Typhoeus::Request.new(url, header)
-      response = request.run
-    end
+  def make_request(url, header)
+    request = Typhoeus::Request.new(url, header)
+    response = request.run
   end
 
+  def json_parse(url, key = nil)
+    JSON.parse(make_request(url, headers: {Authorization: "token #{token}"}).body).map do |to_parse| 
+      key ? to_parse[key] : to_parse
+    end
+  end
 
 end
